4fa610e67497b2ad6532a72eee657aebceeb34aa
endymion1818 <endymion1818@gmail.com>
2024-04-25 15:35:38 +0100

    example of cypress limitation

--- ___E2E___/should-render.spec.js
+++ ___E2E___/should-render.spec.js
@@ -8,6 +8,7 @@ describe("template spec", () => {
   it("should render", () => {
     cy.get("media-player#my-player").should("exist");
   });
+  // ✅ works as expected
   it("should play", () => {
     cy.get("media-player#my-player").
       shadow().
@@ -17,4 +18,12 @@ describe("template spec", () => {
     cy.get("media-player#my-player")
       .should("have.attr", "mediaplaying", "true");
   });
+  // ❌ doesn't re-render the component so test fails
+  it('should render a different video', () => {
+    cy.get("media-player#my-player")
+      .invoke('attr', 'videourl', 'https://www.w3schools.com/tags/movie.mp4')
+      .shadow()
+      .find('video')
+      .should('have.attr', 'src', 'https://www.w3schools.com/tags/movie.mp4')
+  })
 });
