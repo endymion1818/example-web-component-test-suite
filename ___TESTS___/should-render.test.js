b6a053c17520f3e5d91a5a4bd79cb3d6df6898bc
endymion1818 <endymion1818@gmail.com>
2024-04-25 15:42:15 +0100

    example failures for jsdom

--- ___TESTS___/should-render.test.js
+++ ___TESTS___/should-render.test.js
@@ -7,8 +7,22 @@ describe("<media-player> ", () => {
   beforeEach(() => {
     init();
   });
+  // ✅ works as expected
   it("should render", async () => {
     document.body.innerHTML = '<media-player></media-player>';
     expect(document.querySelector('media-player')).to.exist;
   });
+  // ❌ doesn't re-render the component so test fails
+  it("should play the video", async () => {
+    document.body.innerHTML = '<media-player></media-player>';
+    const controlBar = document.querySelector('media-player')?.shadowRoot().querySelector('media-control-bar')
+    controlBar?.shadowRoot().querySelector('media-play-button')?.click();
+    expect(controlBar).to.have.attribute('mediahasplayed', 'true');
+  });
+  it('should render a different video', () => {
+    document.body.innerHTML = '<media-player videourl="https://www.w3schools.com/tags/movie.mp4"></media-player>';
+    const mediaPlayer = document.querySelector('media-player');
+    const video = mediaPlayer?.shadowRoot().querySelector('video');
+    expect(video).to.have.attribute('src', 'https://www.w3schools.com/tags/movie.mp4');
+  })
 });
